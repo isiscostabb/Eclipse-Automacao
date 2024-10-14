1fcc683ec6dea96af573150b5a7db0591e302bc0
Isis Costa <isiscostabezbirolo@gmail.com>
2024-10-14 15:27:16 -0300

    otimização planetas 3D

--- eclipseautomacao/src/components/Eclipse.jsx
+++ eclipseautomacao/src/components/Eclipse.jsx
@@ -5,11 +5,10 @@ import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 
 // Função para carregar o modelo 3D do Sol
-function SunModel({position, setsunTargetPosition}) {
+function SunModel({ position, setsunTargetPosition }) {
   const { scene } = useGLTF('./public/sun_model.glb');
   const ref = useRef();
   const speed = 0.09; // Velocidade
-
 
   // Animação sol
   useFrame(() => {
@@ -34,7 +33,6 @@ function MoonModel({ position, targetPosition }) {
   useFrame(() => {
     if (ref.current) {
       ref.current.position.lerp({ x: targetPosition[0], y: targetPosition[1], z: targetPosition[2] }, speed);
-      ref.current.rotation.y += 0.001; // Rotação
     }
   });
 
@@ -42,22 +40,30 @@ function MoonModel({ position, targetPosition }) {
 }
 
 function Eclipse() {
-  const [moonPosition, setMoonPosition] = useState([-3.4, 25, -17]); //Entrada LUA
+  const [moonPosition, setMoonPosition] = useState([-3.4, 25, -17]); // Entrada LUA
   const [targetPosition, setTargetPosition] = useState([-3.4, 20, -17]); // Final LUA
 
-  const [sunPosition, setsunPosition] = useState([-200, -800, -1000]); //Entrada SOL
+  const [sunPosition, setsunPosition] = useState([-200, -800, -1000]); // Entrada SOL
   const [sunTargetPosition, setsunTargetPosition] = useState([-200, -400, -1000]); // Final SOL
+
+  const [hasScrolled, setHasScrolled] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollY = window.scrollY; // Posição atual do scroll
+
+      // "Trava" apoós o scroll de 700px
+      if (scrollY > 700 && !hasScrolled) {
+        setHasScrolled(true);
+      }
+
       const maxScroll = document.body.scrollHeight - window.innerHeight; // Máximo de scroll disponível
       const scrollPercentage = scrollY / maxScroll; // Porcentagem de scroll feita
 
       // Atualiza a posição da lua
       const newTargetX = -3.7 + scrollPercentage;
       const newTargetY = -9;  
-      const newTargetZ = -18 + scrollPercentage ;
+      const newTargetZ = -18 + scrollPercentage;
       setTargetPosition([newTargetX, newTargetY, newTargetZ]);
     };
 
@@ -68,7 +74,7 @@ function Eclipse() {
       // Remove o evento de scroll ao desmontar o componente
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [hasScrolled]);
 
   return (
     <div className='animacaoEclipse'>
@@ -76,13 +82,13 @@ function Eclipse() {
         <h1 className='titulo'>ECLIPSE AUTOMAÇÃO</h1>
         <h2 className='subtitulo'>SOLUÇÕES DE AUTOMAÇÃO INDUSTRIAL</h2>
       </div>
-      <Canvas camera={{ position: [0.4, -0.12, 2], fov: 50 }}>
+      <Canvas camera={{ position: [0.4, -0.12, 2], fov: 50 }} frameloop={hasScrolled ? "demand" : "always"}>
         <Suspense fallback={null}>
           <ambientLight />
-          <SunModel position={sunPosition} setsunTargetPosition={sunTargetPosition}/>
+          <SunModel position={sunPosition} setsunTargetPosition={sunTargetPosition} />
           <MoonModel position={moonPosition} targetPosition={targetPosition} />
-          </Suspense>
-        </Canvas>
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
