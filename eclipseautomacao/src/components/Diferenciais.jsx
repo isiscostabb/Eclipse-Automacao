c210218bffe9338a2ebcec5af2d88e2f193078bb
Isis Costa <isiscostabezbirolo@gmail.com>
2025-01-01 11:15:43 -0300

    correção cores

--- eclipseautomacao/src/components/Diferenciais.jsx
+++ eclipseautomacao/src/components/Diferenciais.jsx
@@ -14,31 +14,31 @@ function Diferenciais() {
 
       <div className="diferenciais">
         <div className="conteinerDiferenciais">
-            <FaArrowTrendUp size={100}/>
+            <FaArrowTrendUp size={100} color={'#050428'}/>
             <p className='titleDiferenciais'>EXPECTATIVAS</p>
             <p className='pDiferenciais'>Não só queremos atender as demandas de nossos clientes, mas almejamos superar suas expectativas </p>
         </div>
 
         <div className="conteinerDiferenciais">
-            <MdOutlineSecurity size={100}/>
+            <MdOutlineSecurity size={100} color={'#050428'}/>
             <p className='titleDiferenciais'>SOLUÇÕES</p>
             <p className='pDiferenciais'>Proporcionar soluções com eficácia, sustentabilidade e segurança</p>
         </div>
 
         <div className="conteinerDiferenciais">
-            <BiSolidFactory size={100}/>
+            <BiSolidFactory size={100} color={'#050428'}/>
             <p className='titleDiferenciais'>COMPETIVIDADE</p>
             <p className='pDiferenciais'>Promover e elevar a competividade dos nossos clientes</p>
         </div>
         
         <div className="conteinerDiferenciais">
-            <FaHandsHelping size={100}/>
+            <FaHandsHelping size={100} color={'#050428'}/>
             <p className='titleDiferenciais'>HUMANIZADO</p>
             <p className='pDiferenciais'>Comprometimento social e humano, priorizando o respeito mútuo e a ética</p>
         </div>
 
         <div className="conteinerDiferenciais">
-            <FaPeopleRobbery size={100}/>
+            <FaPeopleRobbery size={100} color={'#050428'}/>
             <p className='titleDiferenciais'>AMBIENTE</p>
             <p className='pDiferenciais'>Proporcionar um ambiente agradável de relações e trabalho</p>
         </div>
