78820f95cb75d1da8162d1a72b62ba6b497d6f76
Isis Costa <isiscostabezbirolo@gmail.com>
2025-01-02 17:27:03 -0300

    img Trabalhos

--- eclipseautomacao/src/components/Trabalhos.jsx
+++ eclipseautomacao/src/components/Trabalhos.jsx
@@ -1,6 +1,5 @@
-
 import { IoCloseSharp } from "react-icons/io5";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Trabalhos.css';
 
 const opcoesData = [
@@ -9,43 +8,34 @@ const opcoesData = [
         nome: 'Painéis de automação',
         imgDefault: 'https://i.pinimg.com/736x/8b/68/a9/8b68a99a9ee1905a12b887a592cf92e4.jpg',
         imgHover: 'https://i.pinimg.com/736x/a2/e0/f2/a2e0f22a9d61d49319f75897605c8048.jpg',
-        imagens: ['https://i.pinimg.com/736x/8b/68/a9/8b68a99a9ee1905a12b887a592cf92e4.jpg']
-    },
-    /*{
-        id: 'dois',
-        nome: 'Grupo2',
-        imgDefault: '/public/Grupo2.png',
-        imgHover: '/public/Grupo2-2.png',
-        imagens: ['public/Grupo2/imgGrupo2(1).jpg', 'public/Grupo2/imgGrupo2(2).jpg', 'public/Grupo2/imgGrupo2(3).jpg', 'public/Grupo2/imgGrupo2(4).jpg']
-    },
-    {
-        id: 'tres',
-        nome: 'Grupo3',
-        imgDefault: '/public/Grupo3.png',
-        imgHover: '/public/Grupo3-2.png',
-        imagens: ['']
-    },
-    {
-        id: 'quatro',
-        nome: 'Grupo4',
-        imgDefault: '/public/Grupo4.png',
-        imgHover: '/public/Grupo4-2.png',
-        imagens: ['']
-    }*/
+        imagens: ['https://i.pinimg.com/736x/78/c8/07/78c8071d4ba196be977bbbb6168df305.jpg','https://i.pinimg.com/736x/08/cf/4a/08cf4a50d12978e7107c21e7a599b72f.jpg', 'https://i.pinimg.com/736x/1e/ec/a1/1eeca1793ab90fe093964f88766c8eca.jpg', 'https://i.pinimg.com/736x/2b/25/ff/2b25ffa5390316e72f91b2454a394526.jpg']
+    }
 ];
 
 const Trabalhos = () => {
     const [isAbsoluteVisible, setIsAbsoluteVisible] = useState(false);
-
     const [selectedOption, setSelectedOption] = useState(null);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-    //Opções
+    // Alterna a imagem a cada 5 segundos
+    useEffect(() => {
+        if (selectedOption) {
+            const intervalId = setInterval(() => {
+                setCurrentImageIndex((prevIndex) => 
+                    (prevIndex + 1) % selectedOption.imagens.length
+                );
+            }, 3000);
+
+            return () => clearInterval(intervalId); // Limpa o intervalo ao desmontar
+        }
+    }, [selectedOption]);
+
     const handleOpcaoClick = (opcao) => {
         setSelectedOption(opcao);
         setIsAbsoluteVisible(true);
+        setCurrentImageIndex(0); // Reseta o índice ao abrir
     };
 
-    //Ver mais
     const handleCloseClick = () => {
         setIsAbsoluteVisible(false);
         setSelectedOption(null);
@@ -73,18 +63,23 @@ const Trabalhos = () => {
                         </div>
                     ))}
                 </div>
-                    
+
+                {/* DENTRO */}    
                 <div className={`absolute ${isAbsoluteVisible ? '' : 'fechado'}`}>
                     <div className='fechar' onClick={handleCloseClick}>
                         <IoCloseSharp size={50} color="#050428"/>
                     </div>
+
                     {selectedOption && (
                         <>
-                            <h1>{selectedOption.nome}</h1>
+                            <h1 className="h1Absolute">{selectedOption.nome}</h1>
+                            
                             <div className='imagensTrabalho'>
-                                {selectedOption.imagens.map((imgSrc, index) => (
-                                    <img key={index} src={imgSrc} alt={`Imagem ${index + 1} de ${selectedOption.nome}`} className='ts'/>
-                                ))}
+                                <img 
+                                    src={selectedOption.imagens[currentImageIndex]} 
+                                    alt={`Imagem ${currentImageIndex + 1} de ${selectedOption.nome}`} 
+                                    className='fotos'
+                                />
                             </div>
                         </>
                     )}
