57c53869a8a9d46684c0643ef34790d2bba029b7
Isis Costa <isiscostabezbirolo@gmail.com>
2025-01-02 20:29:46 -0300

    links img trabalho

--- eclipseautomacao/src/components/Trabalhos.jsx
+++ eclipseautomacao/src/components/Trabalhos.jsx
@@ -8,7 +8,22 @@ const opcoesData = [
         nome: 'Painéis de automação',
         imgDefault: 'https://i.pinimg.com/736x/8b/68/a9/8b68a99a9ee1905a12b887a592cf92e4.jpg',
         imgHover: 'https://i.pinimg.com/736x/a2/e0/f2/a2e0f22a9d61d49319f75897605c8048.jpg',
-        imagens: ['https://i.pinimg.com/736x/78/c8/07/78c8071d4ba196be977bbbb6168df305.jpg','https://i.pinimg.com/736x/08/cf/4a/08cf4a50d12978e7107c21e7a599b72f.jpg', 'https://i.pinimg.com/736x/1e/ec/a1/1eeca1793ab90fe093964f88766c8eca.jpg', 'https://i.pinimg.com/736x/2b/25/ff/2b25ffa5390316e72f91b2454a394526.jpg']
+        imagens: ['https://i.pinimg.com/736x/78/c8/07/78c8071d4ba196be977bbbb6168df305.jpg',
+                    'https://i.pinimg.com/736x/08/cf/4a/08cf4a50d12978e7107c21e7a599b72f.jpg',
+                    'https://i.pinimg.com/736x/1e/ec/a1/1eeca1793ab90fe093964f88766c8eca.jpg',
+                    'https://i.pinimg.com/736x/2b/25/ff/2b25ffa5390316e72f91b2454a394526.jpg',
+                    'https://i.pinimg.com/736x/7c/e3/ff/7ce3ffa051627ea1eacf9417d3045969.jpg',
+                    'https://i.pinimg.com/736x/31/19/fb/3119fb98ee9938647344b8e7a0a79c1c.jpg',
+                    'https://i.pinimg.com/736x/57/78/67/577867bba91f95499a6de2d5d1147e61.jpg',
+                    'https://i.pinimg.com/736x/8a/0f/a4/8a0fa484b4a19f1194fb16bd57dcbb04.jpg',
+                    'https://i.pinimg.com/736x/bf/40/3d/bf403dbf53c108f15f9440cf01c9211f.jpg',
+                    'https://i.pinimg.com/736x/a9/70/3d/a9703d3928ee7767f8e82454809afb3c.jpg',
+                    'https://i.pinimg.com/736x/f6/04/33/f60433eb33b669b84a335eb3cb3735d2.jpg',
+                    'https://i.pinimg.com/736x/f9/7b/f3/f97bf3e63eecf81bb91fb3b31d51a144.jpg',
+                    'https://i.pinimg.com/736x/71/b0/2f/71b02f1984d1def5ea91c4eb31259a84.jpg',
+                    'https://i.pinimg.com/736x/41/06/76/41067652ca892c162b777ba6c1109c12.jpg',
+                    'https://i.pinimg.com/736x/41/57/35/4157355a4bf6475cab90c9967df804aa.jpg',
+                    'https://i.pinimg.com/736x/2d/ae/bb/2daebb721a07179e9e0aa6ae48dd53fb.jpg']
     }
 ];
 
